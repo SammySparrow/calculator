56c1616734be7e7442b5a2b980e096f12e3dee31
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-08-09 16:08:27 +1000

    Implement event delegation
    Added switch cases for each button, reduced multiple
    functions into an object.

--- script.js
+++ script.js
@@ -1,33 +1,155 @@
-let numOne;
-let operator;
-let numTwo;
-let input;
+const buttons = document.querySelectorAll("button");
+const interface = document.querySelector("#buttons");
+const display = document.querySelector("#screen");
+const dOne = document.createElement("div");
+const dTwo = document.createElement("div");
+const op = document.createElement("div");
 
-function add(a, b) {
-  return a + b;
-}
+const input = {
+  numOne: [],
+  operator: [],
+  numTwo: [],
+};
 
-function subtract(a, b) {
-  return a - b;
-}
+interface.addEventListener("click", (e) => {
+  let target = e.target;
 
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function operate(first, op, second) {
-  switch (op) {
+  switch (target.id) {
+    case "1":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(1);
+      } else {
+        input.numOne.push(1);
+      }
+      break;
+    case "2":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(2);
+      } else {
+        input.numOne.push(2);
+      }
+      break;
+    case "3":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(3);
+      } else {
+        input.numOne.push(3);
+      }
+      break;
+    case "4":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(4);
+      } else {
+        input.numOne.push(4);
+      }
+      break;
+    case "5":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(5);
+      } else {
+        input.numOne.push(5);
+      }
+      break;
+    case "6":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(6);
+      } else {
+        input.numOne.push(6);
+      }
+      break;
+    case "7":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(7);
+      } else {
+        input.numOne.push(7);
+      }
+      break;
+    case "8":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(8);
+      } else {
+        input.numOne.push(8);
+      }
+      break;
+    case "9":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(9);
+      } else {
+        input.numOne.push(9);
+      }
+      break;
+    case "0":
+      if (
+        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
+      ) {
+        input.numTwo.push(0);
+      } else {
+        input.numOne.push(0);
+      }
+      break;
     case "+":
-      return add(first, second);
+      input.operator.push("+");
+      break;
     case "-":
-      return subtract(first, second);
+      input.operator.push("-");
+      break;
     case "*":
-      return multiply(first, second);
+      input.operator.push("*");
+      break;
     case "/":
-      return divide(first, second);
+      input.operator.push("/");
+      break;
+    case "=":
+      input.push("=");
+      break;
+    case "clear":
+      input.numOne = [];
+      input.operator = [];
+      input.numTwo = [];
+      break;
   }
-}
+  console.log(input);
+
+  dOne.textContent = input.numOne.join("");
+  op.textContent = input.operator.join("");
+  dTwo.textContent = input.numTwo.join("");
+
+  display.appendChild(dOne);
+  display.appendChild(op);
+  display.appendChild(dTwo);
+});
+
+const operations = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+};
+
+console.log(operations.add(2, 3));
