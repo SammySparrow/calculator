35391f5de3e9b70dffc3f264ebaf6d4f09ab4082
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-08-09 16:50:14 +1000

    Complete basic function
    Variables for both numbers and operators are now stored
    correctly, and are used as arguments with the operator
    functions. All relevant variables are displayed on
    screen.

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const display = document.querySelector("#screen");
 const dOne = document.createElement("div");
 const dTwo = document.createElement("div");
 const op = document.createElement("div");
+const outcome = document.createElement("div");
+let result;
 
 const input = {
   numOne: [],
-  operator: [],
+  operator: undefined,
   numTwo: [],
 };
 
@@ -16,125 +18,134 @@ interface.addEventListener("click", (e) => {
 
   switch (target.id) {
     case "1":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(1);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(1);
+      } else {
+        input.numTwo.push(1);
       }
       break;
     case "2":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(2);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(2);
+      } else {
+        input.numTwo.push(2);
       }
       break;
     case "3":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(3);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(3);
+      } else {
+        input.numTwo.push(3);
       }
       break;
     case "4":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(4);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(4);
+      } else {
+        input.numTwo.push(4);
       }
       break;
     case "5":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(5);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(5);
+      } else {
+        input.numTwo.push(5);
       }
       break;
     case "6":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(6);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(6);
+      } else {
+        input.numTwo.push(6);
       }
       break;
     case "7":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(7);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(7);
+      } else {
+        input.numTwo.push(7);
       }
       break;
     case "8":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(8);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(8);
+      } else {
+        input.numTwo.push(8);
       }
       break;
     case "9":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(9);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(9);
+      } else {
+        input.numTwo.push(9);
       }
       break;
     case "0":
-      if (
-        input.operator.some((o) => o == "+" || o == "-" || o == "*" || o == "/")
-      ) {
-        input.numTwo.push(0);
-      } else {
+      if (input.operator === undefined) {
         input.numOne.push(0);
+      } else {
+        input.numTwo.push(0);
       }
       break;
     case "+":
-      input.operator.push("+");
+      input.operator = "+";
       break;
     case "-":
-      input.operator.push("-");
+      input.operator = "-";
       break;
     case "*":
-      input.operator.push("*");
+      input.operator = "*";
       break;
     case "/":
-      input.operator.push("/");
+      input.operator = "/";
       break;
     case "=":
-      input.push("=");
+      if (
+        input.numOne == [] ||
+        input.operator == undefined ||
+        input.numTwo == []
+      ) {
+        break;
+      } else {
+        let tempOne = input.numOne.join("");
+        let tempTwo = input.numTwo.join("");
+        switch (input.operator) {
+          case "+":
+            result = operations.add(Number(tempOne), Number(tempTwo));
+            break;
+          case "-":
+            result = operations.subtract(Number(tempOne), Number(tempTwo));
+            break;
+          case "*":
+            result = operations.multiply(Number(tempOne), Number(tempTwo));
+            break;
+          case "/":
+            result = operations.divide(Number(tempOne), Number(tempTwo));
+            break;
+        }
+        input.numOne = [];
+        input.operator = undefined;
+        input.numTwo = [];
+      }
       break;
     case "clear":
       input.numOne = [];
-      input.operator = [];
+      input.operator = undefined;
       input.numTwo = [];
+      result = "";
       break;
   }
   console.log(input);
 
   dOne.textContent = input.numOne.join("");
-  op.textContent = input.operator.join("");
+  op.textContent = input.operator;
   dTwo.textContent = input.numTwo.join("");
+  outcome.textContent = result;
 
   display.appendChild(dOne);
   display.appendChild(op);
   display.appendChild(dTwo);
+  display.appendChild(outcome);
 });
 
 const operations = {
